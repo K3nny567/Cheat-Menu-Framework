0ed4d13eaff16367ac77c5bc245862e729f28a1a
K3nny567 <144315629+K3nny567@users.noreply.github.com>
2024-05-06 11:16:12 +1000

    Update 30_CheatModule_InvEdit.rb
    -fixed crash on empty items

--- Modules/Inventory Editor/addons/30_CheatModule_InvEdit.rb
+++ Modules/Inventory Editor/addons/30_CheatModule_InvEdit.rb
@@ -51,7 +51,7 @@ class Window_DebugItem < Window_Command
     end
     for i in 1...group.size
       text = sprintf(fmt, i)
-      add_command(text, :item, true, group[i]) if !(group[i].item_name.nil? or group[i].item_name == "" or group[i].description == "")
+      add_command(text, :item, true, group[i]) if !(group[i].nil? or group[i].item_name.nil? or group[i].item_name == "" or group[i].description == "")
     end
   end
 
