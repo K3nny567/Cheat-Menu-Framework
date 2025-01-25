8ec4a7e7ad300730edcfc418c7e2a97d40b67fa4
Kenny Yo <144315629+K3nny567@users.noreply.github.com>
2025-01-25 23:14:49 +1100

    refactor: Mod Update v1.0-rc.11
    -[new] Mod configs stored in `"UserData/cheatmenu/"` folder.
    -[change] Mod Config file is now located at `"UserData/cheatmenu/config.ini"`
    -[change] CheatModule Load Order file is now located at `"UserData/cheatmenu/loadorder.json"`
    -[removed] Internal handling of mod text and resource paths. `getText` and `getResources` functions now fully rely on `$game_text` and `$mod_manager.get_resource` respectively.
    -[reverted] Mod Text split between `"text"` and `"ModText"` folders. The two folders have been merged back together.
    
    BREAKING CHANGE: Due to changes in this update, a clean install of the mod is required.

--- Framework/__init__.rb
+++ Framework/__init__.rb
@@ -8,24 +8,37 @@
 class CheatsMod
   attr_reader :version
   attr_reader :config
+  attr_reader :configDir
   attr_reader :loadorder
   attr_reader :path
-  attr_reader :text
   attr_reader :modid
   attr_accessor :modules
   attr_accessor :addons
   attr_accessor :hotkey
 
   def initialize
-    @version = '1.0-rc.10.1'
+    @version = '1.0-rc.11'
     @config = nil
+    @configDir = nil
     @addons = nil
     @loadorder = nil
     @modid = "cheatmenu"
-    @path = File.dirname(__FILE__)
-    @text = Text.new("#{@path}/ModText/#{$lang}") #unless @umm
+    @path = $mod_manager.mods[@modid].path
     @modules = {}
     @hotkey = nil
+  end
+
+  def init_configDir
+    @configDir = System_Settings::USER_DATA_PATH} + @modid
+
+    # Check if the directory exists
+    unless Dir.exist?(@configDir)
+      # Create the directory if it doesn't exist
+      Dir.mkdir(@configDir)
+      puts "Directory created: #{@configDir}"
+    else
+      puts "Directory already exists: #{@configDir}"
+    end
   end
 
   def init_config(ini_file)
@@ -44,7 +57,7 @@ class CheatsMod
   end
 
   def init_loadorder(file)
-    unless File.exist?(getResource("#{@modid}", "#{file}"))
+    unless File.exist?(file)
       default_loadorder = [
           "UnlockTool", "UnequipItems", "InvEdit", "Summons",
           "Race", "Pregnancy", "StatsEdit", "HairColorEdit",
@@ -52,29 +65,25 @@ class CheatsMod
           "InfiniteMainStats", "AutoBandage", "AutoClean", "InfiniteMoney"
         ]
       
-      File.open(getResource("#{@modid}", "#{file}"), 'w') {
+      File.open(file, 'w') {
         |lo_file|
 
         lo_file.write(JSON.encode(default_loadorder))
         lo_file.close
       }
     end
-    json_file = File.open(getResource("#{@modid}", "#{file}"))
+    json_file = File.open(file)
     @loadorder = JSON.decode(json_file.read())
     @loadorder << :rest
     json_file.close
   end
 
-  def updateText
-    @text = Text.new("#{@path}/ModText/#{$lang}") #unless @umm
-  end
-
   def getText(text_flag)
-    return @text[text_flag]
+    return $game_text["#{@modid}:#{text_flag}"]
   end
 
   def getResource(id, resource)
-    return "#{@path}/#{resource}"
+    return $mod_manager.get_resource(id, resource)
   end
 
   #Include a single script
@@ -97,8 +106,11 @@ end
 if $mod_cheats.nil?
   $mod_cheats = CheatsMod.new
 
+  # Initialize Mod Config (Check config folder exists, create if not exist)
+  $mod_cheats.init_configDir
+
   #Import Load Order (creates default if no file)
-  $mod_cheats.init_loadorder("loadorder.json")
+  $mod_cheats.init_loadorder("#{$mod_cheats.configDir}/loadorder.json")
 
   #Include Plugins class
   $mod_cheats.import("scripts", "Plugins")
@@ -112,7 +124,7 @@ if $mod_cheats.nil?
   #Include project
   $mod_cheats.import("scripts", "Utils") # CheatUtils
   $mod_cheats.import("scripts", "Config") # Cheat Config
-  $mod_cheats.init_config("UserData/GameCheats.ini")
+  $mod_cheats.init_config("#{$mod_cheats.configDir}/config.ini")
   $mod_cheats.config.readHotkey
   $mod_cheats.import("scripts", "Menu") # Cheat Menu
   
--- Framework/scripts/lib/6_TextLangUpdate.rb
+++
@@ -1,8 +0,0 @@
-module DataManager
-  self.singleton_class.send(:alias_method, :update_Lang_CHEATMENUFRAMEWORK, :update_Lang)
-
-	def self.update_Lang
-    	update_Lang_CHEATMENUFRAMEWORK
-		$mod_cheats.updateText
-	end
-end
